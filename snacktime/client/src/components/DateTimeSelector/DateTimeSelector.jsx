8e6fdbc39af02af4b02c8e4a45dac2e4d6b7cd9c
Onion Rings <liangbryan2@gmail.com>
2018-10-08 20:47:53 -0700

    bryan

--- snacktime/client/src/components/DateTimeSelector/DateTimeSelector.jsx
+++ snacktime/client/src/components/DateTimeSelector/DateTimeSelector.jsx
@@ -1,53 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
-  },
+    width: 200
+  }
 });
 
-class DateTimeSelector extends React.Component {
-  state = {
-    time: "",
-  }
-handleChange = event => {
-  this.setState({
-    [name]: event.target.value,
-  });
-  this.props.setNap(this)
+function toJSONLocal(date) {
+  var local = new Date(date);
+  local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  return local.toJSON().slice(0, 16);
 }
 
-  const { classes } = this.props;
+var d = new Date(); // 
+var n = toJSONLocal(d);
+// console.log(n);
+class DateTimeSelector extends React.Component {
+  state = {
+    time: n
+  };
+
+  handleChange = name => event => {
+    console.log("event.target.value", event.target.value);
+    this.setState({
+      [name]: event.target.value
+    });
+    this.props.setNap(this.state.time);
+  };
+
   render() {
-  return (
-    <form className={classes.container} noValidate>
-      <TextField
-        label={props.label}
-        id="datetime-local"
-        type="datetime-local"
-        name="time"
-        onChange={this.handleChange}
-        value={this.state.time}
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-    </form>
-  );
+    const { classes } = this.props;
+    console.log(this.props.setNap)
+    return (
+      <form className={classes.container} noValidate>
+        <TextField
+          id="datetime-local"
+          label={this.props.label}
+          type="time"
+          name="time"
+          onChange={this.handleChange("time")}
+          value={this.state.time}
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </form>
+    );
+  }
 }
 
 DateTimeSelector.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(DateTimeSelector);
