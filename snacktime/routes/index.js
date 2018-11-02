635dcffd49a5a3f38090170382976c5d65ebc727
BQ <qian.brian@gmail.com>
2018-11-01 18:02:35 -0700

    test

--- snacktime/routes/index.js
+++ snacktime/routes/index.js
@@ -4,13 +4,13 @@ const apiRoutesStaff = require('./api/staff.js');
 const apiRoutesParent = require('./api/parent.js');
 const authRoutes = require('./auth/auth.js');
 
-
 router.use('/api', apiRoutesStaff);
 router.use('/api', apiRoutesParent);
 router.use('/auth', authRoutes);
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  console.log('serving index.html');
 });
 
 module.exports = router;
