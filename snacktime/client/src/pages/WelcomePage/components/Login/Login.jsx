c9f72c268717a849174afec7aaa0b123f2c2c5d7
BQ <qian.brian@gmail.com>
2018-11-01 20:09:57 -0700

    changed login to history push

--- snacktime/client/src/pages/WelcomePage/components/Login/Login.jsx
+++ snacktime/client/src/pages/WelcomePage/components/Login/Login.jsx
@@ -1,90 +1,90 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import LockIcon from "@material-ui/icons/LockOutlined";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import withStyles from "@material-ui/core/styles/withStyles";
-import './Login.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { withRouter } from 'react-router';
+import './Login.css';
 
 const styles = theme => ({
   layout: {
-    width: "auto",
-    display: "block", // Fix IE11 issue.
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
       width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`
+      .spacing.unit * 3}px`,
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE11 issue.
-    marginTop: theme.spacing.unit
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit,
   },
   submit: {
-    marginTop: theme.spacing.unit * 3
-  }
+    marginTop: theme.spacing.unit * 3,
+  },
 });
 
 class LogIn extends React.Component {
   state = {
-    email: "",
-    password: "",
-    statusText: ""
+    email: '',
+    password: '',
+    statusText: '',
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("login props", this.props);
+    console.log('login props', this.props);
     fetch(`/auth/login/${this.props.type}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
-        password: this.state.password
-      })
+        password: this.state.password,
+      }),
     }).then(res =>
       res
         .text()
         .then(res => {
-          console.log(res === "Success");
-          if (res === "Success") {
+          console.log(res === 'Success');
+          if (res === 'Success') {
             if (this.props.oldLocation) {
-              window.location.href = `${this.props.oldLocation}`;
+              this.props.history.push(`${this.props.oldLocation}`);
+              // window.location.href = `${this.props.oldLocation}`;
             } else {
-              window.location.href = `/${this.props.type}HomePage`;
+              this.props.history.push(`/${this.props.type}HomePage`);
+              // window.location.href = `/${this.props.type}HomePage`;
             }
           }
           // 'Email does not exist in our database'
-          else if (res === "Email does not exist in our database") {
+          else if (res === 'Email does not exist in our database') {
             this.setState({
-              statusText: "Email does not exist in our database"
+              statusText: 'Email does not exist in our database',
             });
           }
           // 'Incorrect Password'
-          else if (res === "Incorrect Password") {
-            this.setState({ statusText: "Incorrect Password" });
+          else if (res === 'Incorrect Password') {
+            this.setState({ statusText: 'Incorrect Password' });
           }
         })
         .catch(err => console.log(err))
@@ -94,66 +94,64 @@ class LogIn extends React.Component {
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({
-      [name]: value
+      [name]: value,
     });
   };
 
   render() {
     const { classes } = this.props;
     return (
-
-        <main className={classes.layout}>
-          <Paper className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <LockIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+                autoComplete="email"
+                autoFocus
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                onChange={this.handleChange}
+                value={this.state.password}
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </FormControl>
+            <div className="errormsg">{this.state.statusText}</div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+              className={classes.submit}
+            >
               Sign in
-            </Typography>
-            <form className={classes.form}>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  onChange={this.handleChange}
-                  value={this.state.email}
-                  autoComplete="email"
-                  autoFocus
-                />
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input
-                  onChange={this.handleChange}
-                  value={this.state.password}
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </FormControl>
-              <div className="errormsg">{this.state.statusText}</div>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color='primary'
-                onClick={this.handleSubmit}
-                className={classes.submit}
-              >
-                Sign in
-              </Button>
-              
-            </form>
-          </Paper>
-        </main>
+            </Button>
+          </form>
+        </Paper>
+      </main>
     );
   }
 }
 
 LogIn.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(LogIn);
+export default withStyles(styles)(withRouter(LogIn));
