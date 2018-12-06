234620c8abfd382808dff1e081b8137ba2822f3c
Onion Rings <liangbryan2@gmail.com>
2018-12-05 20:31:48 -0800

    fix white pages

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 app.use(
