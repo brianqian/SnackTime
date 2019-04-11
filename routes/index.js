46cd11f4690a04105a7b36a3f4e08ca90f7483b2
BQ <qian.brian@gmail.com>
2019-04-11 15:42:55 -0700

    changes production routes

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,21 @@ const path = require('path');
 const apiRoutesStaff = require('./api/staff.js');
 const apiRoutesParent = require('./api/parent.js');
 const authRoutes = require('./auth/auth.js');
-
+const express = require('express');
 
 router.use('/api', apiRoutesStaff);
 router.use('/api', apiRoutesParent);
 router.use('/auth', authRoutes);
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
-
+if (process.env.NODE_ENV === 'production') {
+  router.use(express.static('client/build'));
+  router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+} else {
+  router.use(express.static(__dirname + 'client/public'));
+  router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
+}
 module.exports = router;
--- server.js
+++ server.js
@@ -11,15 +11,12 @@ const PORT = process.env.PORT || 3001;
 const db = require('./models');
 
 // Define middleware here
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
-app.use(bodyParser.json({limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
-}
 
 app.use(
   session({
